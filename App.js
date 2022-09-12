6257d39956f45d52060d12df0faea3f75dcbb0ec
liiamarika <liia.hanninen@myy.haaga-helia.fi>
2022-09-12 17:21:08 +0300

    Recipe finder

--- App.js
+++ App.js
@@ -1,13 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, Alert, Image, KeyboardAvoidingView } from 'react-native';
 
 export default function App() {
+
+const [ingrd, setIngrd] = useState('');
+const [recipes, setRecipes] = useState([]);
+
+const getRecipes = () => {
+  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=` + ingrd;
+  fetch(url)
+  .then ( response => response.json() )
+  .then ( responseJson => setRecipes(responseJson.meals) )
+  .catch((error) => {
+    Alert.alert('Error', error);
+  });
+}
+
+const listSeparator = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View
+      style={{
+        height: 1,
+        width: "80%",
+        backgroundColor: "#CED0CE",
+        marginLeft: "10%"
+      }}
+    />
+  );
+};
+
+  return (
+    
+   <KeyboardAvoidingView 
+    style={styles.container}
+    behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <View style={styles.listContainer}>
+        <FlatList
+        data={recipes}
+        keyExtractor={item => item.idMeal }
+        renderItem= {
+          ({item}) => 
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', padding: 7, width: 300}}>
+            <Text>{item.strMeal}</Text>
+            <Image style={{  width:20, height:20 }} 
+            source={{  uri: '' + item.strMealThumb + '/preview' }} />
+          </View>  }
+        ItemSeparatorComponent={listSeparator}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput style={{width:200, borderColor:'gray', borderWidth:1}} 
+        value={ingrd}
+        onChangeText={(ingrd) => setIngrd(ingrd)} />
+        <Button title="find" 
+        onPress = {getRecipes}/>
+      </View>
+    
+    </KeyboardAvoidingView>
   );
 }
 
@@ -15,7 +64,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  inputContainer: {
+    flex: 2,
+    marginTop: 50,
+    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    
+  },
+  listContainer: {
+    flex: 5,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    marginTop: 50
   },
 });
